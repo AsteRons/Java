335c8687561953e7e67893ed21cb875a7bf6e9d4
AsteRons <r.maciek1995@o2.pl>
2018-02-13 22:56:57 +0100

    +
    +

---
+++ Part1_1/src/part1_1/Part1_1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package part1_1;
+
+/**
+ *
+ * @author Maciek
+ */
+public class Part1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int t = 2;
+        int z = 4;
+        int w = t + z;
+        
+        
+        
+        System.out.println(w);  //komentarz
+    }
+    
+}
