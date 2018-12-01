025158ba85797e1a5ad1f5f8ebc92f7d4648acf9
AsteRons <r.maciek1995@o2.pl>
2018-12-01 13:35:34 +0100

    +

---
+++ Books/Book_Java_Fundamentals/1_Fundamental_Programming_Structures_in_Java/Part_1/src/Part_1/Part_1.java
@@ -0,0 +1,16 @@
+/**
+ * Pierwszy program Java
+ * podstawy
+ */
+
+package Part_1;
+
+public class Part_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+	}
+
+}
+ 
