dfcaca40c5b8a11fb29fd482108924ee3110a14e
AsteRons <r.maciek1995@o2.pl>
2018-12-02 12:39:41 +0100

    +

--- Books/Book_Java_Fundamentals/1_Fundamental_Programming_Structures_in_Java/Part_1/src/Part_1/Part_1.java
+++ Books/Book_Java_Fundamentals/1_Fundamental_Programming_Structures_in_Java/Part_1/src/Part_1/Part_1.java
@@ -10,6 +10,13 @@ public class Part_1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi");
+		
+		final int aTrap = 1;	//inicjalizowanie sta³ych
+		
+		// Rzutowanie
+		
+		double x = 9.91;
+		int t = (int) Math.round(x);
 	}
 
 }
