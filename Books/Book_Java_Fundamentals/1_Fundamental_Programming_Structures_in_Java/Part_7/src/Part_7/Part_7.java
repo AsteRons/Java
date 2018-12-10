0e9b3951ce5db4818af65d6580ad1be8474bbfcb
AsteRons <r.maciek1995@o2.pl>
2018-12-10 20:34:35 +0100

    up
    up

--- Books/Book_Java_Fundamentals/1_Fundamental_Programming_Structures_in_Java/Part_7/src/Part_7/Part_7.java
+++ Books/Book_Java_Fundamentals/1_Fundamental_Programming_Structures_in_Java/Part_7/src/Part_7/Part_7.java
@@ -8,3 +8,4 @@ public class Part_7 {
 	}
 
 }
+ 
---
+++ Courses/Udemy_JAVA_courses/Zmienne/Zmienne/src/Zmienne/Zmienne.java
@@ -0,0 +1,22 @@
+package Zmienne;
+
+public class Zmienne {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 2;
+		int c;
+		double t;
+		c = a + b;
+		String nazwa = "nazwa";
+		System.out.print(c);
+		System.out.println(nazwa + " ");
+		
+		
+		//Rzutowanie
+		t = (double)a;
+		
+	}
+
+}
