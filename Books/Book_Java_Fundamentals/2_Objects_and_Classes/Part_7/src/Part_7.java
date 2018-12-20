f0c4c0ce7684154b7c1b735b7e7212262f7ab431
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2018-12-20 09:29:02 +0100

    up
    up

--- Books/Book_Java_Fundamentals/2_Objects_and_Classes/Part_7/src/Part_7.java
+++ Books/Book_Java_Fundamentals/2_Objects_and_Classes/Part_7/src/Part_7.java
@@ -13,7 +13,26 @@ class Employe{
 	
 	//Inicjalizacja pól
 	
+	private static int nexstId;
 	private String name = "";
 	private double salary = 0;
 	
+	
+	public Employe(String n, double s)
+	{
+		name = n;
+		salary = s;
+	}
+	
+	public Employe(double salary)
+	{
+		this("Employe #" + nexstId, salary);
+		nexstId++;
+	}
+	
+	public Employe()
+	{
+		name   = "" ;
+		salary = 0;	
+	}
 }
