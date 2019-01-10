4cc4ebb3e5af2bbadbf81c9500e4ccb551c9061a
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-10 11:36:47 +0100

    up
    up

--- Courses/Udemy_JAVA_courses_PL/6_Programowanie_obiektowe/Part_3/src/Part_3/Part_3.java
+++ Courses/Udemy_JAVA_courses_PL/6_Programowanie_obiektowe/Part_3/src/Part_3/Part_3.java
@@ -3,8 +3,62 @@ package Part_3;
 public class Part_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		KontoBankowe oszczednosciowe = new KontoBankowe();
+		if(oszczednosciowe.wyplac(i))
+		{
+			System.out.println("wyp³acono");
+			
+		}
+		else
+			System.out.println("za malo kasy");
+		
+		System.out.println(oszczednosciowe.getSaldo());
+		
+		
+		
 	}
 
+}
+
+
+class KontoBankowe
+{
+	
+	private int saldo;
+	
+	public KontoBankowe()
+	{
+		saldo = 1000;
+		
+	}
+	
+	
+	
+	int getSaldo()
+	{
+		return saldo;
+		
+	}
+	
+	void setSaldo(int saldo)
+	{
+		this.saldo = saldo;
+	}
+	
+	
+	
+	boolean wyplac(int ile)
+	{
+		if( saldo < ile)
+			return false;
+		else
+			setSaldo(saldo - ile);
+			
+		return true;
+
+	}
+	
+	
 }
--- Courses/Udemy_JAVA_courses_PL/6_Programowanie_obiektowe/README_Part_3.txt
+++ Courses/Udemy_JAVA_courses_PL/6_Programowanie_obiektowe/README_Part_3.txt
@@ -1,2 +1,5 @@
 ﻿ENKAPSULACJA:
 
+- tworzenie obiektu oraz metod
+- brak mozliwosci modyfikowania pol z zewnątrz klasy
+- starac sie robic duzo malych klas i je wykorzystywac
