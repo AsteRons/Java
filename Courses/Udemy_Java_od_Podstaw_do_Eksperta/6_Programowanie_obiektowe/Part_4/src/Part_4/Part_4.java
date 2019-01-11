bfb24aec72d6a6fca9b8a9d3292f36d6a238b201
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-11 14:33:58 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_4/src/Part_4/Part_4.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_4/src/Part_4/Part_4.java
@@ -4,7 +4,24 @@ public class Part_4 {
 
 	public static void main(String[] args) {
 		
-
+		
+		
+		int a = 20;
+		
+		Test x = new Test();	//x nie znajduje sie w zadna wartosc tylko adres
+		x.zmienWartosc(a);
+		
 	}
 
+}
+
+
+class Test
+{
+	
+	
+	void zmienWartosc(int zm)
+	{
+		zm = zm + 2;	// kopia która przestaje istnieæ
+	}
 }
