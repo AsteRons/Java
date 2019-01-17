3c3a1c66c1db9cb4440c07b8606b1aab001b1331
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-17 11:07:45 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_11/src/Part_11/Part_11.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_11/src/Part_11/Part_11.java
@@ -1,10 +1,134 @@
 package Part_11;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.lang.*;
+
 public class Part_11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		// Interfejsy
+		
+		nazwaInterfejsu a = new Pracownik();
+				
+		((Pracownik)a).getWynagrodzenie();
+		
+		// Sortowanie
+		
+		int[] tab = new int[3];
+		
+		tab[0] = 5;
+		tab[1] = 1;
+		tab[2] = 4;
+		
+		
+		
+		Arrays.sort(tab);			//sortowanie rosn¹co
+			
+		
+		
+		Pracownik[] pracownik = new Pracownik[3];
+		pracownik[0] = new Pracownik(1000);
+		pracownik[1] = new Pracownik(500);
+		pracownik[2] = new Pracownik(2000);
+		
+		System.out.println("Przed sortowaniem: ");
+		for(Pracownik p : pracownik)
+		{
+			System.out.println(p.getWynagrodzenie());
+		}
+		
+		Arrays.sort(pracownik);
+		
+		
+		System.out.println("Po sortowaniem rosn¹co: ");
+		for(Pracownik p : pracownik)
+		{
+			System.out.println(p.getWynagrodzenie());
+		}
+		
+		Arrays.sort(pracownik, Collections.reverseOrder());
+		
+		
+		System.out.println("Po sortowaniem malej¹co: ");
+		for(Pracownik p : pracownik)
+		{
+			System.out.println(p.getWynagrodzenie());
+		}
+		
 	}
+
+}
+
+
+
+
+
+interface nazwaInterfejsu
+{
+	double PI = 3.14;  // public static final
+	
+	void cos();	       //public abstract
+
+	
+	
+}
+
+
+
+
+class Pracownik implements nazwaInterfejsu, Comparable<Object>
+{
+	
+	
+	private double wynagrodzenie;
+	
+	
+	
+	Pracownik()
+	{
+		
+	}
+	
+	Pracownik(double wynagrodzenie)
+	{
+		this.wynagrodzenie = wynagrodzenie;
+	}
+	
+
+	public double getWynagrodzenie()
+	{
+		return this.wynagrodzenie;
+	}
+
+
+	public void cos() {
+		
+		
+	}
+
+	
+	public int compareTo(Object o) {
+		
+		
+		Pracownik przyslany = (Pracownik)o;
+		
+		if(this.wynagrodzenie < przyslany.wynagrodzenie)
+		
+			return -1;
+			
+		
+		if (this.wynagrodzenie > przyslany.wynagrodzenie)
+			return 1;
+		
+		
+		return 0;
+		
+	}
+
+	
 
 }
---
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/README_Part_11.txt
@@ -0,0 +1,21 @@
+INTERFEJSY:
+
+- wszystkie metody publiczne i abstrakcyjne
+- pola publiczne finalne i statyczne
+
++ tworzy sie je aby zimplementowac pewne zachowania
+
++ mogê implementowac wiele interfejsów i tylko jedn¹ klase 
+
+SORTOWANIE:
+
+- sortowanie tablicy liczb
+- sortowanie obiektów
+
+
+- interfejs Comparable
+ + implementacja tego interfejsu w swojej klasie
+ 
+ sort - mo¿emy uzywac do obiektów gdy zinterpretujemy metode compareTo (rosn¹co)
+	+ sortowanie malej¹co z wykorzystaniem Collections.reverseOrder()
+ 
