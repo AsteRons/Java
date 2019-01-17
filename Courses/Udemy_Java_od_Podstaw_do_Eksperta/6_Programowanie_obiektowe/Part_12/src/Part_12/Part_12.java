812b806ef36ffdb7c182cde5fb5d5baf3d950a76
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-17 11:39:22 +0100

    Part_11, Part_12,
    add examples Java OO

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_12/src/Part_12/Part_12.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_12/src/Part_12/Part_12.java
@@ -3,8 +3,47 @@ package Part_12;
 public class Part_12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		A zewnetrzna = new A();
+		
+		A.B wew= zewnetrzna.new B();
 
 	}
 
+}
+
+
+class A
+{
+	
+	A()
+	{
+		System.out.println("Jestem z kalsy A");
+	}
+	
+	class B
+	{
+		B()
+		{
+			System.out.println("Jestem z kalsy B");
+		}
+		
+		void coœ()
+		{
+			test = 5;
+		}
+		
+		private int tmp;
+	}
+	
+	
+	private int test;
+	
+	
+	void coœ2()
+	{
+		//tmp = 5;     - nie ma do tego dostepu
+	}
+	
 }
---
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/README_Part_12.txt
@@ -0,0 +1,13 @@
+KLASY ZAGNIEŻDŻONE:
+
+- statyczna klasa zagniezdżona oraz zwykła klasa zagniezdżona
++ klasa wewnetrzna ma dostepo do wewnętrzna ma dostęp do priv klasy zewnętrznej
++ klasa zew. nie ma dostępu do pól i metod klasy wewnetrznej
+
+- aby zrobic klase wew trzeba stworzyc obiekt klase zew w nim zrobić klase wew
+
+Klasa STATYCZNA:
+
+- klasa statyczna może istniec bez klasy zewnętrznej
+- klasa statyczna nie ma dostępu do pól kalsy zewnętrznej
+- możemy tworzyć pola statyczne
