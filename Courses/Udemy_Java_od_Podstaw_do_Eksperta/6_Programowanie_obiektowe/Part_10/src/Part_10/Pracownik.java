f6c83ce32a2723d21ae0ff08acd654b8779ccd97
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-17 09:15:59 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_10/src/Part_10/Pracownik.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/Part_10/src/Part_10/Pracownik.java
@@ -1,6 +1,25 @@
 package Part_10;
+import java.util.Date;
+
 
 public class Pracownik extends Osoba{
 
+
+	final Date	dataZatrudnienia;
+	
+	Pracownik()
+	{
+		this.dataZatrudnienia = new Date();
+	}
+	
+	
+	Pracownik(Date date)
+	{
+		this.dataZatrudnienia = date;
+	}
+	
 	
 }
+
+
+
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/README_Part_10.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/6_Programowanie_obiektowe/README_Part_10.txt
@@ -2,4 +2,15 @@
 
 Final:
 
+
+
 - klase finalna nie mozna rozszeżyć:
+- ustawianie np. dnia zatrudnienia
+
+Finalne pole;
+- nie można zmienic, takie same dla wszytsich obiektów stworzonych przez tą klase
+
+
+Fjnalna metoda:
+
+- finalna metode nie da rady nadpisać
