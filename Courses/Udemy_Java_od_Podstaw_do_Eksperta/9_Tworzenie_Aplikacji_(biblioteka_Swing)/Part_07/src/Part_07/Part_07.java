8b5f95f716aa6fb1dabe9dcbc952b52472102bf2
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-24 12:11:17 +0100

    Update Part_07.java
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_07/src/Part_07/Part_07.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_07/src/Part_07/Part_07.java
@@ -1,9 +1,32 @@
 package Part_07;
 
-public class Part_07 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.*;
 
+
+public class Part_07 extends JFrame {
+
+	public Part_07()
+	{
+		super("Zegarek");
+		this.setBounds(300, 300, 300, 200);
+		initComponents();
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	}
+	
+	public void initComponents()
+	{
+		
+		
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new Part_07().setVisible(true);
 
 	}
 
