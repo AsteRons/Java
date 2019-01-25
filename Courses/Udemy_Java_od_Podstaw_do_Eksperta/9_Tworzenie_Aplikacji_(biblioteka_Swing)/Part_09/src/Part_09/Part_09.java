9303842bbe1313ff357fa2b59a016fddc2e1af33
AsteRons <r.maciek1995@o2.pl>
2019-01-25 19:38:04 +0100

    Part_10
    MENU

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_09/src/Part_09/Part_09.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_09/src/Part_09/Part_09.java
@@ -12,7 +12,10 @@ public class Part_09 extends JFrame {
 	JPanel panel1 = new JPanel();
 	JPanel panel2 = new JPanel();
 	
+	JLabel etykieta = new JLabel("jestem piekna");
 	
+	JCheckBox pogrubiony = new  JCheckBox("Pogrubiony");
+	JCheckBox pochylony = new  JCheckBox("Pochylony");	
 	
 	/*
 	 * Konstruktor domyslny
@@ -28,15 +31,56 @@ public class Part_09 extends JFrame {
 	 * Inicjalizacja komponentow
 	 */
 	
+	
 	public void initComponents()
 	{
 		this.setTitle("Pole Wyboru");
 		this.setBounds(500, 500 ,500, 400);
 		
+		etykieta.setFont(new Font("Monospaced", Font.PLAIN, 20));
 		
-		this.getContentPane().add(, constraints);
+		panel2.add(etykieta);
+		
+		ActionListener sluchacz = new CheckBoxHandler();
+		
+		pogrubiony.addActionListener(sluchacz);
+		pochylony.addActionListener (sluchacz);
+		
+		
+		panel1.add(pogrubiony);
+		panel1.add(pochylony);
+		
+		this.getContentPane().add(panel1, BorderLayout.NORTH);
+		this.getContentPane().add(panel2, BorderLayout.CENTER);
+		
+		
 		
 	}
+	
+/*
+ * Klasa do ³apania klikniêæ w chackboxach	
+ */
+	
+	public class CheckBoxHandler implements ActionListener
+	{
+
+
+		public void actionPerformed(ActionEvent e) {
+			
+			int style = Font.PLAIN;
+			
+			
+			if(pogrubiony.isSelected()) style += Font.BOLD;
+			if(pochylony.isSelected())  style += Font.ITALIC;
+			
+			
+			etykieta.setFont(new Font(etykieta.getFont().getFontName(), style, etykieta.getFont().getSize()));
+			
+		}
+		
+	}
+	
+	
 	
 	
 	/*
@@ -44,7 +88,7 @@ public class Part_09 extends JFrame {
 	 */
 	public static void main(String[] args) {
 		
-
+	new Part_09().setVisible(true);
 
 	}
 
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_09.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_09.txt
@@ -1,2 +1,9 @@
 ﻿POLE WYBORU - CHECKBOX:
 
+- służy do wybierania np. czcionki
+- tworzy się go za pomocą klasy JCheckBox
+- tworzymy nasłuchiwacz
+	+ można stworzyć jedną klase nasłuchiwacza i za pomocą metody isSelect wybierać co się stanie gdy naciśniemy dany przycisk
+	
+	+ dodajemy poprzez += oznacza to, ze może być i pogrubiony i pochylony
+	
