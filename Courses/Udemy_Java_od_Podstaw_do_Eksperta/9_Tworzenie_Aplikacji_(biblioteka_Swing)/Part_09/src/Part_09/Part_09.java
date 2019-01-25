371c1e33ec2c43fd8906d8cca5009f19d043ac22
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-25 12:29:47 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_09/src/Part_09/Part_09.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_09/src/Part_09/Part_09.java
@@ -1,9 +1,41 @@
 package Part_09;
 
-public class Part_09 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Part_09 extends JFrame {
+
+	/*
+	 * Konstruktor domyslny
+	 */
+	
+	public Part_09()
+	{
+		initComponents();
+		
+	}
+	
+	/*
+	 * Inicjalizacja komponentow
+	 */
+	
+	public void initComponents()
+	{
+		this.setTitle("Pole Wyboru");
+		this.setBounds(500, 500 ,500, 400);
+		
+		
+		
+	}
+	
+	
+	/*
+	 * Metoda wykonawcza, domyslna
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
 
 	}
 
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_09.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_09.txt
@@ -1,1 +1,2 @@
-﻿
+﻿POLE WYBORU - CHECKBOX:
+
