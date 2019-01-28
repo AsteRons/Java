0da06c0be4dc95215fc06ddecffcdf7406508113
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-28 15:40:10 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_12/src/Part_12/Part_12.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_12/src/Part_12/Part_12.java
@@ -16,17 +16,61 @@ public class Part_12 extends JFrame{
 		this.setTitle("Przeszukiwacz Tekstu");
 		this.setBounds(500, 500, 500, 400);
 		
+		panelSzukania.add(znajdz);
+		panelSzukania.add(szukany);
+		
+		// obszarTekstowy.selectAll();  			   	//- zaznacza ca³y tekst
+		// obszarTekstowy.select(0, 2);	   			     // - zaznacza obszar odk¹d - dok¹d
+		// obszarTekstowy.replaceSelection("lala");		 //- zamieniæ obszar, który aktualnie jest zaznaczony
+		// obszarTekstowy.replaceRange("Lala", 0, 2);	// - podbieniamy w tekscie o danym zasiêgu na string Lala
+		// obszarTekstowy.insert("La", 0);				 //- dodaje tekst w okreslonym miejscu
+     	// obszarTekstowy.append("Ja");				//- do³¹cza do koñca obszaru podany tekst
+		// obszarTekstowy.select(obszarTekstowy.getText().indexOf("test"), obszarTekstowy.getText().indexOf("test")+ "test".length());
+				
+		znajdz.addActionListener(new Znajdowanie() );
+		
+		
 		this.getContentPane().add(obszarPrzewijania, BorderLayout.NORTH);	
+		this.getContentPane().add(panelSzukania, BorderLayout.CENTER);	
+		pack();
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
 
 	
-	private  JTextArea obszarTekstowy = new JTextArea(7, 1_0);
+	private  JTextArea obszarTekstowy = new JTextArea( "To jest testowy tekst, w testowym obszarze", 10, 30);
 	private JScrollPane  obszarPrzewijania = new JScrollPane(obszarTekstowy);
+	private JPanel panelSzukania = new JPanel();
+	private JButton znajdz = new JButton("Szukaj");
+	private JTextField szukany = new JTextField(8);
 	
 	public static void main(String[] args) {
 		new Part_12().setVisible(true);
 
 	}
+	
+	private class Znajdowanie implements ActionListener
+	{
+
+		private int poczatekSzukanego = 0;
+		
+		public void actionPerformed(ActionEvent e) {
+			
+			// Wypisuje mi numer pocz¹tku wpisanego tekstu
+			// Jesli nie znalaz³o wypisuje -1 
+			poczatekSzukanego = obszarTekstowy.getText().indexOf(szukany.getText());
+			
+			System.out.println(poczatekSzukanego );
+			
+			if(poczatekSzukanego >= 0 && szukany.getText().length() > 0) 
+			{
+				obszarTekstowy.requestFocus();
+				obszarTekstowy.select(poczatekSzukanego, poczatekSzukanego + szukany.getText().length());
+			}
+			
+		}
+		
+	}
+	
 
 }
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_12.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_12.txt
@@ -21,3 +21,31 @@ Klasa do tworzenia suwaka do przewijania:
 		
 
 
+Ciekawe metody na obszaże tekstowym:
+- selectAll() - zaznacza wszytsko na obszarze tekstowym
+- select(x, y) - zaznacza fragment tekstu
+	x - początek
+	y - koniec 
+	
+- replaceSelection(x) - zamienia zaznaczony fragment na x typu String
+
+- replaceRange(x, y, z) - zamienia tekstod y do z na tekst x (String) 
+
+- insert(x, y) - dodanie Stringa x od miejsca y
+- append(x)		- dodaje na końcu obszaru tekst x o typie String
+
+- select(x, y) - wyszukuje z naszego obszaru i zaznacza
+	x (obszarTekstowy.getText().indexOf("test") - od początku napisu test pobranego z obszaru tekstowego
+	y - do jakiego obszaru np. długośc tego tekstu
+	
+	Metoda do zaznaczania słówka "test":
+- obszarTekstowy.select(obszarTekstowy.getText().indexOf("test"), obszarTekstowy.getText().indexOf("test")+ "test".length());
+
+Tworzymy program do szukania wyrazów
+1. Tworzymy przycisk szukaj
+2. Tworzymy JTextField oraz ustawiamy jego wielkosć
+3. Tworzymy nasłuchiwacz na to pole
+4. Tworzymy metodę do wypisuwaniu numeru początku wpisanego tekstu do pola JTextField
+5. Sprawdzamy czy w polu cos jest wpisane oraz czy długosć wpisanego tekstu jest większa od 0
+6. Ustawienie skupienia na obszaze tekstowym
+7. Zaznaczenie tekstu od poczatekSzukanego do poczatekSzukanego + długosć tekstu
