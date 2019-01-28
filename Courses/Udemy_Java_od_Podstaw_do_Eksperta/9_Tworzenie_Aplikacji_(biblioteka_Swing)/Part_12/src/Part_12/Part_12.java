7addf7d0004100d8725c2ff6ed0804d6d93a0557
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-28 11:53:11 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_12/src/Part_12/Part_12.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_12/src/Part_12/Part_12.java
@@ -1,9 +1,31 @@
 package Part_12;
 
-public class Part_12 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Part_12 extends JFrame{
+
+	public Part_12()
+	{
+		initComponents();
+		
+	}
+	
+	private void initComponents() {
+		this.setTitle("Przeszukiwacz Tekstu");
+		this.setBounds(500, 500, 500, 400);
+		
+		this.getContentPane().add(obszarPrzewijania, BorderLayout.NORTH);	
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	
+	private  JTextArea obszarTekstowy = new JTextArea(7, 1_0);
+	private JScrollPane  obszarPrzewijania = new JScrollPane(obszarTekstowy);
+	
 	public static void main(String[] args) {
-
+		new Part_12().setVisible(true);
 
 	}
 
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_12.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_12.txt
@@ -1,2 +1,23 @@
 ﻿OBSZAR TEKSTOWY, PRZESZUKIWANIE I PODMIENIANIE TEKSTU:
 
+- Tworzenie obszaru tekstowego
+- Przeszukuje obszar tekstowy i ewentualnie zamienia tekst
+
+Obszar tekstowy tworzymy za pomoca klasy:
+- JTextArea
+
++ (Wygląda podobnie jak notatnik)
+
+Klasa do tworzenia suwaka do przewijania: 
+- JScrollPane
+
+1. Stworzony obszar tekstowy dodajemu do konstruktora klasy JScrollPane
+2. Obiekt stworzony przez klase JScrollPane dodajemy do panelu
+3. Tak stworzony obszar możemy  na nim pisac o0gra go przewijac
+
+	- w trakcie dodawania do panelu możemy ustawić layout
+	- W trakcie tworzenia konstruktora obszaru tekstowego
+		możemy ustawić liczbę kolumn i wierszy
+		
+
+
