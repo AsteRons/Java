ab7a7833d67c3d948441076d56c726c940505b62
Maciej Rudnik <m.rudnik@cmgi.com.pl>
2019-01-30 10:35:36 +0100

    Update Part_13.java
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_13/src/Part_13/Part_13.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_13/src/Part_13/Part_13.java
@@ -1,10 +1,32 @@
 package Part_13;
 
-public class Part_13 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+
+public class Part_13 extends JFrame{
+
+	private JPanel panel = new JPanel();
+	
+	public Part_13()
+	{
+		initComponents();
+
+	}
+
+
+	public void initComponents()
+	{
+		this.setTitle("Listy kombinowane");
+		this.setBounds(500, 500, 500, 400);
+	
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new Part_13().setVisible(true);
 	}
 
 }
