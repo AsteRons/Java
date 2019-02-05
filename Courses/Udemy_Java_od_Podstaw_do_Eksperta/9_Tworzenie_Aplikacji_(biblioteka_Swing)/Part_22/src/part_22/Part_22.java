6a3765ca4139bd0eb05e3b4d63538046427fec00
AsteRons <r.maciek1995@o2.pl>
2019-02-05 20:53:23 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_22/src/part_22/Part_22.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/Part_22/src/part_22/Part_22.java
@@ -12,11 +12,12 @@ import java.awt.event.*;
 public class Part_22 extends JFrame{
 
     JToolBar pasekNarzedzi = new JToolBar("Nazwa nowej ramki");
-    
+    JButton button = new JButton("Wyłącz malowanie");
     
     public Part_22()
     {
         initComponents();
+       
     }
     
    private void initComponents()
@@ -25,14 +26,73 @@ public class Part_22 extends JFrame{
        this.setTitle("Pasek Narzędzi");
        this.setBounds(500, 500, 500, 400);
       
-       pasekNarzedzi.add(new JButton("Lala"));
+       pasekNarzedzi.add(new KolorowyButton(new ActionColor("Zmieniam kolor na zielony", new ImageIcon("zielony.gif"), Color.GREEN )));
+       pasekNarzedzi.add(new KolorowyButton(new ActionColor("Zmieniam kolor na niebieski", new ImageIcon("niebieski.gif"), Color.BLUE )));       
+       pasekNarzedzi.add(new KolorowyButton(new ActionColor("Zmieniam kolor na czerwony", new ImageIcon("czerwony.gif"), Color.RED )));       
+       pasekNarzedzi.add(button);
        this.getContentPane().add(pasekNarzedzi, BorderLayout.NORTH);
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    }
+  
    
    
+   // Klasa do tworzenia akcji przycisku
+   private class ActionColor extends AbstractAction
+   {
+       public ActionColor(String toolTip, Icon icon, Color colour)
+       {
+           this.putValue(Action.SHORT_DESCRIPTION, toolTip);
+           this.putValue(Action.SMALL_ICON, icon);
+           this.putValue("kolor", colour);
+           
+           
+       }
+        public void actionPerformed(ActionEvent ae) {
+  
+        }
+
+   }
    
-    
+   //Klasa do tworzenia przycisku
+    private class KolorowyButton extends JButton
+    {
+        private KolorowyButton(final ActionColor actionColor) {
+            
+            super(actionColor);
+
+            this.addActionListener(new ActionListener(){
+              
+                
+                public void actionPerformed(ActionEvent ae) {
+                   
+                    for(int i = 0; i < pasekNarzedzi.getComponentCount(); i++)
+                    {
+                        
+                       if(pasekNarzedzi.getComponent(i) instanceof KolorowyButton)
+                       {
+                           KolorowyButton temp = (KolorowyButton)pasekNarzedzi.getComponent(i);
+                       }
+                      
+                    }
+                    
+                   ten.setBackground((Color)actionColor.getValue("kolor"));
+                   ten.setZaznacziny(true);
+                }
+
+            });
+        }
+        
+        
+        // Funkcja do zmieniania flagi
+        public void setZaznacziny(boolean zaznacz)
+        {
+        this.zaznaczony = zaznacz;
+        }
+        
+        KolorowyButton ten = this;
+        boolean zaznaczony = false; 
+    }
+
     public static void main(String[] args) {
 
      new Part_22().setVisible(true);
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_22.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/9_Tworzenie_Aplikacji_(biblioteka_Swing)/README_Part_22.txt
@@ -1,14 +1,30 @@
 ﻿PASEK NARZĘDZI I KURSORY:
 
 Program uczy:
-1. Jak zmienić pasek narzędzi
+
+I. Jak zmienić pasek narzędzi
 	
 	- JToolBar - klasa do tworzenia paska narzędzi
      	+ Zachowuje się odpowiednio do Layoutu
 		+ Możemy dodawać przyciski za pomocą add
-
-
-2. Jak zmienić kursor na własny
+	
+	1. Tworzymy klase do obsługi kliknięcia z 
+		- opisem
+		- ikonami
+		- kolorem
+		
+	2. Tworzymy klase do tworzenia przycisków	
+		- tworzymy konstruktor z nadklasy
+		- tworzymy zdażenie które po wciśnięciu zmienia tło danego przycisku na kolor.
+		
+	3. Tworzymy metodę do ustawiania flagi
+	
+	- getComponent - pobiera komponent np. z paska narzędzi
+	- getComponentCount - liczy komponenty np. na pasku narzędzi
+	-  X instanceof Y - czy obiekt X nalezy do instancji klasy Y
+	
+	
+II. Jak zmienić kursor na własny
 
 
 
