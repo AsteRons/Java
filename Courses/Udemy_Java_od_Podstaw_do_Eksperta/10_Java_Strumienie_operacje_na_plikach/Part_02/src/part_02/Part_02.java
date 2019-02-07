d64d2b5ef1f9326b927ee86a906ff9f3768b4b4f
AsteRons <r.maciek1995@o2.pl>
2019-02-07 12:44:34 +0100

    UP
    UP

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_02/src/part_02/Part_02.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_02/src/part_02/Part_02.java
@@ -1,10 +1,38 @@
 package part_02;
 
+import java.io.*;
+
 public class Part_02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+	
+		File plik = new File("nazwaPliku.txt");
+	
+		InputStream inS;
+		OutputStream outS;
+		
+		Reader reader;
+		/*
+		 * 
+		 * 
+		 */
+		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(plik)));
+		
+	
+		
+		((BufferedWriter)writer).write("lala");
+		
+	    ((BufferedWriter)writer).newLine();
+	    
+	    ((BufferedWriter)writer).write("Nastêpna linia.");
+	    ((BufferedWriter)writer).close();
+		
+		}
+	
+	void nazwaF (InputStream inS)
+	{
+		
 	}
 
 }
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/README_Part_02.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/README_Part_02.txt
@@ -1,2 +1,29 @@
 ﻿CZYM JEST WEJSCIE, WYJSCIE STRUMIENI:
 
+
+
+
+ABSTRAKCYJNA KLASY STRUMIENI:
+
+Klasy:
+- InputStream	 -	 operujemy na bajtach
+- Reader			 -	 operujemy na znakach
+
+- OutputStream  
+- Writer
+
+FILTROWANIE STRUMIENI:
+1. Tworzenie strumienie
+2. Otwieranie ich
+3. Pisanie w nich jakiś tekst
+4. Przechodzenie w nową linijkę 
+5. 
+
+
+- strumienie można między sobą łączyć
+- BufferedWriter - dzięki temu, możemy połączyć dwa strumienie
+- BufferedWriter - pozwala nam zapisywać coś szybciej
+
+File - stworzenie obiektu pliku
+FileOutputStream - pozwala otworzyć strumien
+OutputStreamWriter - pozwala nam zapisać coś do pliku
