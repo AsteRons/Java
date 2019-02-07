f2dfefc1cef652f6b1bf7866873054050e13e41a
AsteRons <r.maciek1995@o2.pl>
2019-02-07 15:06:09 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_03/src/part_03/Part_03.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_03/src/part_03/Part_03.java
@@ -1,10 +1,83 @@
 package part_03;
 
+import java.io.*;
+
 public class Part_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try {
+			
+			// Zapisywanie do pliku
+			
+			PrintWriter drukarz = new PrintWriter(new FileWriter("dane.txt"));
+			
+			
+			drukarz.println(1234);
+			
+			// drukarz.flush();
+			
+			drukarz.close();
+			
+			
+			drukarz = new PrintWriter(new FileWriter("dane.txt", true));
+			drukarz.append("lala");
+			
+			drukarz.printf("lala %d", 50);
+			
+			drukarz.close();
+			
+			// Czytanie w pliku
+			
+			BufferedReader reader = new BufferedReader (new FileReader("dane.txt"));
+			
+			// Czytanie po jednym znaku
+			
+			// System.out.println((char)reader.read());
+			
+			 // Czytamy po ca³ej linijce
+			 
+			 // System.out.println(reader.readLine());
+			 
+
+			BufferedWriter writer = new  BufferedWriter(new FileWriter("baza.txt"));
+			
+			
+			
+			String tresc = "";
+			
+			// Przepisuje dane z jednego pliku do drugiegi
+			while((tresc = reader.readLine()) != null)
+				{
+
+				writer.write(tresc);
+				writer.newLine();
+				
+				}
+				
+			writer.close();
+			reader.close();
+			
+			
+			
+		}catch(IOException e)
+		{
+		System.out.println(e.getMessage());	
+		}
+		
+		
+	//	Part_03.test(1, 2, 3, 131, "lala", 34);
+		
+		
+		
 	}
 
+	static void test(Object ...  a)
+	{
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+	
 }
--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/README_Part_03.txt
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/README_Part_03.txt
@@ -1,2 +1,38 @@
 ﻿ZAPIS I ODCZYT - SFORMATOWANY TEKST:
 
+- w jaki sposób zapisać oraz odczytać coś z pliku
+
+Klasa do pisania plików:
+
++ PrintWriter  - drukuje reprezentacje obiektu 
+ - do zapisywania czegoś na obiekcie, możemy wykorzystać metodę print();
+ 
+ 
+ new FileWriter(x , y)
+
+x - nazwa pliku String
+y - true/flase - czy do pliku można dopisywać czy tworzyć go jeszcze raz 
+ 
+ + Po wykonaniu jezcze raz programu plik zostanie zresetowany ( usunięte zostaną elementy)
+ 
+ - close()  - strumień należy zamykać ponieważ, mozę z buforu nie wszystko zostanie zapisane
+ - flush() - opróżnia i zapisuje bufor ( np. dobre jak wystąpi błąd)
+ 
+ Możemy zakmknąć a następnie ponownie otworzyć plik i napisać coś.
+ 
+ - append()    - dodanie stringa do łańcucha znaków
+ - printf()   	- 	dodawanie do pliku elementów podobnie jak w C
+				   + 
+				   
+				   
+	Odczyt z pliku:
+	
+	BufferedReader - wykorzystujemy klase ( tworzymy w nim obiekt)
+	
+	Metody:
+	
+	- read()        - czytanie po jednym znaku ( należy zrzutować na char)
+	- readLine()  - czytanie po całej linijce, gdy dojdzie do końca wysyła null
+	
+	- program wczytuje i zapisuje na ekran dane z pliku tekstowego
+	
