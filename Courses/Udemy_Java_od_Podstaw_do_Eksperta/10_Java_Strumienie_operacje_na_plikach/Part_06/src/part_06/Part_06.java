afe4b344cfb524ce95edf5465667d8a34e15f476
AsteRons <r.maciek1995@o2.pl>
2019-02-08 00:52:40 +0100

    up
    up

--- Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_06/src/part_06/Part_06.java
+++ Courses/Udemy_Java_od_Podstaw_do_Eksperta/10_Java_Strumienie_operacje_na_plikach/Part_06/src/part_06/Part_06.java
@@ -1,10 +1,40 @@
 package part_06;
 
+import java.io.*;
+
+
 public class Part_06 {
 
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Towar[] towar = new Towar[3];
+		
+		towar[0] = new Towar();
+		towar[1] = new Towar(29.0, "Video Kurs Java");
+		towar[2] = new Towar(39.0, "Video Jurs C++", 2008, 11, 1);
+		
+		
+		try
+		{
 
+			RandomAccessFile RAF = new  RandomAccessFile("baza.txt", "rw");
+		
+			
+			
+			
+			
+			RAF.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
 	}
 
 }
