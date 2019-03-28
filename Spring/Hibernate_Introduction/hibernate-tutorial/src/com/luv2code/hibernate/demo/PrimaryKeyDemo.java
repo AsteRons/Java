4879915d8c52851acef1876d9b6f26d10a0c8265
AsteRons <r.maciek1995@o2.pl>
2019-03-28 22:47:47 +0100

    up
    up

--- Spring/Hibernate_Introduction/hibernate-tutorial/src/com/luv2code/hibernate/demo/PrimaryKeyDemo.java
+++ Spring/Hibernate_Introduction/hibernate-tutorial/src/com/luv2code/hibernate/demo/PrimaryKeyDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 
 	public static void main(String[] args) {
 
@@ -20,32 +20,31 @@ public class CreateStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {			
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Maciej", "Rudnik", "maciek@luv2code.com");
+			// create 3 student objects
+			System.out.println("Creating 3 student objects...");
+			
+			Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+			Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+			Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
 			
 			// start a transaction
 			session.beginTransaction();
 			
 			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
+			System.out.println("Saving the students...");
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
 			
 			// commit transaction
 			session.getTransaction().commit();
 			
 			System.out.println("Done!");
-		
 		}
 		finally {
-
-			factory.close();
 		}
+		
+		
 	}
 
 }
-
-
-
-
-
--- Spring/Hibernate_Introduction/hibernate-tutorial/src/com/luv2code/hibernate/demo/ReadStudentDemo.java
+++ Spring/Hibernate_Introduction/hibernate-tutorial/src/com/luv2code/hibernate/demo/ReadStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class ReadStudentDemo {
 
 	public static void main(String[] args) {
 
@@ -22,23 +22,42 @@ public class CreateStudentDemo {
 		try {			
 			// create a student object
 			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Maciej", "Rudnik", "maciek@luv2code.com");
+			Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
 			
 			// start a transaction
 			session.beginTransaction();
 			
 			// save the student object
 			System.out.println("Saving the student...");
+			System.out.println(tempStudent);
 			session.save(tempStudent);
 			
 			// commit transaction
 			session.getTransaction().commit();
 			
+			// MY NEW CODE
+			
+			// find out the student's id: primary key
+			System.out.println("Saved student. Generated id: " + tempStudent.getId());
+			
+			// now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// retrieve student based on the id: primary key
+			System.out.println("\nGetting student with id: " + tempStudent.getId());
+			
+			Student myStudent = session.get(Student.class, tempStudent.getId());
+			
+			System.out.println("Get complete: " + myStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
 			System.out.println("Done!");
-		
 		}
 		finally {
-
+			
 			factory.close();
 		}
 	}
