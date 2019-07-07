750752de52a1d17d2d31dbc273349fc774949666
Maciek Dom <maciej.rudnik2@gmail.com>
2019-07-07 21:18:24 +0200

    SpringBoot Guru
    Start Spring Boot Start

--- Spring_Guru/spring5webapp/src/test/java/com/asterons/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ Spring_Guru/spring5webapp/src/test/java/com/asterons/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookstore;
+package com.asterons.springframework.spring5webapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookstoreAngularApplicationTests {
+public class Spring5webappApplicationTests {
 
 	@Test
 	public void contextLoads() {
