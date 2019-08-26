06164fc28ff3f0963fcf92b09fd80dae832db929
Maciek Dom <maciej.rudnik2@gmail.com>
2019-08-26 23:34:45 +0200

    Update Spring Project
    Add:
    - Spring MVC
    -Clinic Project

---
+++ Spring_Guru/asterons/src/main/java/com/asterons/asterons/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package com.asterons.asterons.controllers;
+
+import com.asterons.asterons.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
---
+++ Spring_Guru/sgf-pet-clinic/src/main/java/com/example/sgfpetclinic/SgfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgfPetClinicApplication.class, args);
+    }
+
+}
