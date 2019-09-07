15d885c7123435a2c39749d8703b8b7865eded18
Maciek Dom <maciej.rudnik2@gmail.com>
2019-09-07 16:34:40 +0200

    Spring Guru #3
    Add Profile, Primary

--- Spring_Guru/di-demo/src/main/java/asterons/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ Spring_Guru/di-demo/src/main/java/asterons/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("de")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greting service";
+        return "Primärer Grußdienst";
     }
 }
--- Spring_Guru/di-demo/src/main/java/asterons/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ Spring_Guru/di-demo/src/main/java/asterons/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile({"es", "default"})
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greting service";
+        return "Servicio de Saludo Primario";
     }
 }
