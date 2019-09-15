18489f13e0bd5dc087a7a42ea36b81f8a178ce42
Maciek Dom <maciej.rudnik2@gmail.com>
2019-09-15 19:52:15 +0200

    Spring Guru #10
    Impl Map based Services

---
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/services/map/PetMapService.java
@@ -0,0 +1,34 @@
+package com.asterons.springframe.sfgpetclinic.services.map;
+
+      import com.asterons.springframe.sfgpetclinic.model.Pet;
+      import com.asterons.springframe.sfgpetclinic.services.CrudService;
+      import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void detele(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
---
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/services/map/VetMapService.java
@@ -0,0 +1,34 @@
+package com.asterons.springframe.sfgpetclinic.services.map;
+
+      import com.asterons.springframe.sfgpetclinic.model.Vet;
+      import com.asterons.springframe.sfgpetclinic.services.CrudService;
+      import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void detele(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
