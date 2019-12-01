5de9bc7e865fd74b45fba4170b6047b6f0d4113f
Maciek Dom <maciej.rudnik2@gmail.com>
2019-12-01 23:01:52 +0100

    #123
    fixed CSS

--- Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/test/java/com/asterons/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/test/java/com/asterons/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,8 +1,11 @@
 package com.asterons.springframe.sfgpetclinic;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 
 @SpringBootTest
