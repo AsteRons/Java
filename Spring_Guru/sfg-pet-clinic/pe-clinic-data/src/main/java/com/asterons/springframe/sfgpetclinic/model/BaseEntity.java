22f60aa6488ebb6be3dae87ed4d8908ccd3a24d5
Maciek Dom <maciej.rudnik2@gmail.com>
2019-12-17 21:23:56 +0100

    fixed bug

--- Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/BaseEntity.java
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/BaseEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @MappedSuperclass
-public class BaseEntity implements Serializable {
+public class  BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
