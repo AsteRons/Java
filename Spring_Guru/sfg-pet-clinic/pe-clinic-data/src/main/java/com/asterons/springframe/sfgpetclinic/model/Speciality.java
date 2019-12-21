589007a6a0376c5e83a8333c6be4f5f431bff6e7
Maciek Dom <maciej.rudnik2@gmail.com>
2019-12-21 14:26:38 +0100

    #154

--- Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/Speciality.java
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/Speciality.java
@@ -1,8 +1,20 @@
 package com.asterons.springframe.sfgpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
+
+    public String getDescription() {
+        return description;
+    }
 
     public void setDescription(String description) {
         this.description = description;
