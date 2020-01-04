4cb9f02c5cdc64c055e19754d08cadf6f43fea6e
Maciek Dom <maciej.rudnik2@gmail.com>
2020-01-04 21:33:20 +0100

    #157

--- Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/repositories/OwnerRepository.java
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import com.asterons.springframe.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
+
+
+    Owner findByLastName(String lastName);
 }
