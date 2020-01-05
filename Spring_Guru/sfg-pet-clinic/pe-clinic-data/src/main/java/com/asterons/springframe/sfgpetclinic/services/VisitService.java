5ae7beb3845cbf3b2d035f1cc1519551d0fb1da3
Maciek Dom <maciej.rudnik2@gmail.com>
2020-01-05 16:52:53 +0100

    #163

---
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.asterons.springframe.sfgpetclinic.services;
+
+
+import com.asterons.springframe.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
