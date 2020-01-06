f03e05a21e82a42b89e20949d5be6b9571a63852
Maciek Dom <maciej.rudnik2@gmail.com>
2020-01-06 12:07:31 +0100

    #174

--- Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/PetType.java
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/model/PetType.java
@@ -1,10 +1,17 @@
 package com.asterons.springframe.sfgpetclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
@@ -12,11 +19,4 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
