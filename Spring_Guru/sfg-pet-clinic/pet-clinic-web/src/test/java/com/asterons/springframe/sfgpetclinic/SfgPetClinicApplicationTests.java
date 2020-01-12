ef7e4afd6b8f5ec603ea7461a2c40fa3426d2872
Maciek Dom <maciej.rudnik2@gmail.com>
2020-01-12 16:26:51 +0100

    #188

--- Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/test/java/com/asterons/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/test/java/com/asterons/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
