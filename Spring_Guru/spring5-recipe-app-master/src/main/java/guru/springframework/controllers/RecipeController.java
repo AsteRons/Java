902473e9b8377c07a35e96ffe1e363310fff194d
Maciek Dom <maciej.rudnik2@gmail.com>
2020-02-01 23:19:57 +0100

    205

--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -3,9 +3,9 @@ package guru.springframework.controllers;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import guru.springframework.commands.RecipeCommand;
 
 @Controller
 public class RecipeController {
@@ -20,5 +20,21 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
