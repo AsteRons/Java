5a57957f6daac54178ef745f692acf147ae295cc
Maciek Dom <maciej.rudnik2@gmail.com>
2020-02-02 17:26:55 +0100

    Update a Recipe Test

--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -16,7 +16,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
@@ -35,6 +35,6 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 }
--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/RecipeService.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/RecipeService.java
@@ -9,5 +9,6 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
     Recipe findById(Long l);
+    RecipeCommand findCommandById(Long l);
     RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
