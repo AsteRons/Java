a2bd99a4a291a4b0458a3b252afd0a6a5e747d95
Maciek Dom <maciej.rudnik2@gmail.com>
2020-02-11 22:17:30 +0100

    Show Ingredient

--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -30,6 +30,12 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
         ingredientCommand.setAmount(ingredient.getAmount());
+
+        if(ingredient.getRecipe() != null)
+        {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
+
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
         return ingredientCommand;
