f134215f4deacb3248203dc3152221a5a5c6bf46
Maciek Dom <maciej.rudnik2@gmail.com>
2020-02-14 23:29:49 +0100

    Enumeration Dropdowns with Spring MVC and Thymeleaf

--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -17,13 +17,13 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/{id}/show")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
-    @RequestMapping("/recipe/new")
+    @GetMapping("/recipe/new")
     public String newRecipe(Model model){
 
         model.addAttribute("recipe", new RecipeCommand());
@@ -38,14 +38,14 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @RequestMapping("recipe/{id}/update")
+    @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return  "recipe/recipeform";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/delete")
+
+    @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
 
         log.debug("Deleting id: " + id);
