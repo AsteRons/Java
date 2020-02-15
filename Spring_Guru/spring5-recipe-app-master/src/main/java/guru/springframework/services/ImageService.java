7ed6d8ff938f2e2248dbda394de507495e7b16ef
Maciek Dom <maciej.rudnik2@gmail.com>
2020-02-15 10:53:24 +0100

    Uploading Images with Spring MVC

--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/ImageService.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/ImageService.java
@@ -1,4 +1,9 @@
 package guru.springframework.services;
 
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
 }
--- Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ Spring_Guru/spring5-recipe-app-master/src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,4 +1,17 @@
 package guru.springframework.services;
 
-public class ImageServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        log.debug("Received a file");
+
+    }
 }
