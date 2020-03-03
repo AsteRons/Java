cbcb2c7181c5c69ffe4825b46c434a535d15c568
Maciek Dom <maciej.rudnik2@gmail.com>
2020-03-03 22:54:45 +0100

    edit Pet fix

--- Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/main/java/com/asterons/springframe/sfgpetclinic/controllers/PetController.java
+++ Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/main/java/com/asterons/springframe/sfgpetclinic/controllers/PetController.java
@@ -86,7 +86,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
-            owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
