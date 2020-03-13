41a4f1ce1a4ad9c813e40735acc5c16c496665b3
Maciek Dom <maciej.rudnik2@gmail.com>
2020-03-13 21:03:38 +0100

    Spring Pet Clinic - CRUD Tests Pet Map Service

--- Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/services/map/PetMapService.java
+++ Spring_Guru/sfg-pet-clinic/pe-clinic-data/src/main/java/com/asterons/springframe/sfgpetclinic/services/map/PetMapService.java
@@ -8,9 +8,9 @@ package com.asterons.springframe.sfgpetclinic.services.map;
       import org.springframework.stereotype.Service;
       import java.util.Set;
 
-      @Service
-      @Profile({"default", "map"})
-      public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
+@Service
+@Profile({"default", "map"})
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
