7d5ff61a7340761c016907eb4cc6aa4589c74ff0
Maciek Dom <maciej.rudnik2@gmail.com>
2020-03-13 21:29:37 +0100

    Spring Pet Clinic - Implement Vets List View

--- Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/main/java/com/asterons/springframe/sfgpetclinic/controllers/VetController.java
+++ Spring_Guru/sfg-pet-clinic/pet-clinic-web/src/main/java/com/asterons/springframe/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.asterons.springframe.sfgpetclinic.controllers;
 
+import com.asterons.springframe.sfgpetclinic.model.Vet;
 import com.asterons.springframe.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
