5eb0e44002dbe3e50a57ab47f6d38055dc457716
Maciej <maciej.rudnik2@gmail.com>
2020-03-27 13:12:39 +0100

    add app MongoDB

--- Spring_Guru/spring5-mongo-recipe-app-mongodb-data-model/src/main/java/guru/springframework/domain/Ingredient.java
+++ Spring_Guru/spring5-mongo-recipe-app-mongodb-data-model/src/main/java/guru/springframework/domain/Ingredient.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
-
+import java.util.UUID;
 /**
  * Created by jt on 6/13/17.
  */
@@ -14,8 +14,8 @@ import java.math.BigDecimal;
 @Setter
 public class Ingredient {
 
-    @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
+
     private String description;
     private BigDecimal amount;
 
