09223e58ab2c9ec2d6a9913aef6f542d673c133b
Maciek Dom <maciej.rudnik2@gmail.com>
2020-04-11 11:26:35 +0200

    add webflux

---
+++ Spring_Guru/spring5-reactive-mongo-recipe-app-web-flux-deps/src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface CategoryRepository extends CrudRepository<Category, String> {
+
+    Optional<Category> findByDescription(String description);
+}
