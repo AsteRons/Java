4a8b993b19fca1d3541ea22ba3abf2ba64e19e49
Maciej Rudnik <maciej.rudnik2@gmail.com>
2020-04-23 16:21:39 +0200

    add controller, domain, services

---
+++ Spring_Guru/spring-rest-client-examples-master/src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,17 @@
+package guru.springframework.api.domain;
+
+import java.util.List;
+
+
+public class UserData {
+
+    List<User> data;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
---
+++ Spring_Guru/spring-rest-client-examples-master/src/main/java/guru/springframework/springrestclientexamples/config/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package guru.springframework.springrestclientexamples.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+
+        return builder.build();
+    }
+}
