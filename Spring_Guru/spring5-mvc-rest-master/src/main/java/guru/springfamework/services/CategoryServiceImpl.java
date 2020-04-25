8d77923a59a533321e0457cb3c611a15b09879b7
Maciek Dom <maciej.rudnik2@gmail.com>
2020-04-25 22:43:37 +0200

    List Categories

--- Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -4,11 +4,12 @@ package guru.springfamework.services;
 import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
