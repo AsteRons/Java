7676a4286c4e2f9eed5bfd46ed1d3a91f5daed07
Maciek Dom <maciej.rudnik2@gmail.com>
2020-05-09 17:16:48 +0200

    API URL Value

--- Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by jt on 9/26/17.
  */
 @Controller
-@RequestMapping("/api/v1/categories/")
+@RequestMapping(CategoryController.BASE_URL)
+//@RequestMapping("${some.url.value}")
 public class CategoryController {
 
+    public static final String BASE_URL = "/api/v1/categories";
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
