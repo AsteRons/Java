899ffa114593f2c026beca8447154c6c5f7373f9
Maciek Dom <maciej.rudnik2@gmail.com>
2020-05-10 22:54:38 +0200

    Customizing Swagger with Meta Data

--- Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -1,14 +1,18 @@
 package guru.springfamework.config;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
@@ -21,9 +25,27 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
 
-    
 
-}
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Maciej Rudnik", "https://springframework.guru/about/",
+                "maciej.rudnik2@gmail.com");
+
+        return new ApiInfo(
+                "Spring Framework Guru",
+                " Beginner to Guru",
+                "1.0",
+                "Terms of Service: blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+
+    }
+
+
