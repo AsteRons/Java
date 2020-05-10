8d71ad30146f57811983c0125baabebd160a61ec
Maciek Dom <maciej.rudnik2@gmail.com>
2020-05-10 23:18:51 +0200

    Customizing Endpoint Documention

--- Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,6 +1,8 @@
 package guru.springfamework.api.v1.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CustomerDTO {
 
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstname;
     private String lastname;
+
+    @JsonProperty("customerUrl")
     private String customerUrl;
 
 }
--- Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ Spring_Guru/spring5-mvc-rest-master/src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -4,12 +4,14 @@ package guru.springfamework.controllers.v1;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@Api(description = "This is my Customer Controller")
 @Controller
 @RequestMapping("api/v1/customers/")
 public class CustomerController {
@@ -20,7 +22,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-
+    @ApiOperation(value = "This will get a list of customers", notes = "...")
     @GetMapping
     public ResponseEntity<CustomerListDTO> getListofCustomers(){
 
